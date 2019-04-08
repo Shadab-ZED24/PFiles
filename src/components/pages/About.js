5d6f0c69cad9388e352e510a100714cd4db0a278
Shadab Khan <shadabk128@gmail.com>
2019-04-08 17:08:52 +0530

    Change app.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>This is todo list v1.4.5</p>
+    </React.Fragment>
+  )
+}
